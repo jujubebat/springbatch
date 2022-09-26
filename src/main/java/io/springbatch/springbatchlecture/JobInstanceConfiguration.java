36293578d3858a6c7389688368b67e8809af6e5f
박병욱 <bbwwpark@woowahan.com>
2022-09-26 21:34:09 +0900

    JobInstance 학습

--- src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory JobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
